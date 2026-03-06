74919cb6bd4ac94db1565ba4d596375f52ec80ef
kevinkidtw <kevinkid.tw@gmail.com>
2026-03-06 14:13:21 +0800

    style(web): update default BitTu sprite size to 50

--- www/electron-shim.js
+++ www/electron-shim.js
@@ -114,7 +114,7 @@
                         visible: true,
                         x: 0,
                         y: 0,
-                        size: 15,
+                        size: 50,
                         direction: 90,
                         draggable: false,
                         rotationStyle: 'all around'
